70bb47c6b95eb9c8f33eccf5044c44954e167de4
Daniel Dahlkvist (dice) <daniel.dahlkvist@dice.se>
2014-10-03 16:49:23 +0200

    - Added threading to application - refactoring code
    Signed-off-by: Daniel Dahlkvist (dice) <daniel.dahlkvist@dice.se>

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using dbx_lib;
+using System.Collections.Concurrent;
 
 namespace DbxEntityTracker
 {
@@ -37,43 +38,42 @@ namespace DbxEntityTracker
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string DBX_ROOT = @"E:\rep\ws\FutureData\Source\Gameplay";
+        private string DBX_ROOT = @"E:\rep\ws\FutureData\Source\";
         private string DDF_WSROOT = @"E:\rep\ws\tnt\code\ws";
+        
         public MainWindow()
         {
             InitializeComponent();
             var lib = new LibMain();
-            var allEntities = populateDDF(ref lib);
-            // need to generate "qualified name of entity" i.e. convert CameraDistanceEntity --> WSShared.CameraDistanceEntityData
-            // WSSoldierHealthComponent --> WSShared.WSSoldierHealthComponentData 
+            var startTime = DateTime.Now;
+
+            var ddfFiles = lib.GetFilesOfType(DDF_WSROOT, "ddf");
+            var allEntities = populateDDF(ddfFiles);
+            var ddfTime = DateTime.Now;
             Console.WriteLine("-----------------\n Total Entities: {0}\n-----------------", allEntities.Keys.Count);
 
-            var dbxFilesWithEntities = populateDBX(ref lib);
-            Console.WriteLine("Parsing all dbx-files, found {0} files that might contain entities", dbxFilesWithEntities.Count);
+            var dbxFiles = lib.GetFilesOfType(DBX_ROOT, "dbx");
+            var dbxMatches = populateDBX(dbxFiles);
+            var dbxTime = DateTime.Now;
+            Console.WriteLine("Parsing all dbx-files, found {0} files that might contain entities", dbxMatches.Count);
 
-            var entityUsage = new Dictionary<string, List<string>>(); //Dictionary<EntityReference, DbxFile>
-            foreach (var entity in dbxFilesWithEntities)
-            {
-                foreach (var t in entity.EntityType)
-                {
-                    if (allEntities.Keys.Contains(t))
-                    {
-                        if (entityUsage.ContainsKey(t))
-                            entityUsage[t].Add(entity.Filepath);
-                        else
-                            entityUsage.Add(t, new List<string>() { entity.Filepath });
-                    }
-                }
-            }
+            var entityUsage = crossReferenceEntitiesWithDbxMatches(allEntities, dbxMatches);
+            var mappingTime = DateTime.Now;
+
+            Console.WriteLine("Time to populate ddf files: {0}ms", (ddfTime - startTime).TotalMilliseconds);
+            Console.WriteLine("Time to populate dbx files: {0}ms", (dbxTime - ddfTime).TotalMilliseconds);
+            Console.WriteLine("Time to map dbx files to entities: {0}ms", (mappingTime - dbxTime).TotalMilliseconds);
+
+            var unusedEntities = findUnusedEntities(allEntities, entityUsage);
+            writeUnusedEntities(unusedEntities);
+            int foo = 0;
         }
 
-        private Dictionary<string, string> populateDDF(ref dbx_lib.LibMain lib)
+        private IDictionary<string, string> populateDDF(FileInfo[] ddfFiles)
         {
-            var ddfFiles = lib.GetFilesOfType(DDF_WSROOT, "ddf");
+            var entityTable = new ConcurrentDictionary<string, string>();
 
-            var entityTable = new Dictionary<string, string>();
-
-            foreach (var file in ddfFiles)
+            Parallel.ForEach(ddfFiles, (file) =>
             {
                 using (var sr = new StreamReader(file.FullName))
                 {
@@ -85,8 +85,9 @@ namespace DbxEntityTracker
                         if (!insideEntity && line.StartsWith("entity "))
                         {
                             var name = line.Substring("entity ".Length);
+                            name = name.Split(':')[0].Trim();
                             var fakename = "WSShared." + name + "Data"; //Append something
-                            entityTable.Add(fakename, file.FullName);
+                            entityTable.GetOrAdd(fakename, file.FullName);
                             insideEntity = true;
                         }
                         else if (insideEntity)
@@ -100,18 +101,18 @@ namespace DbxEntityTracker
                         }
                     }
                 }
-            }
+            });
 
-            return entityTable;
+            return new Dictionary<string, string>(entityTable);
         }
 
-        private List<DbxMatches> populateDBX(ref dbx_lib.LibMain lib)
+        private List<DbxMatches> populateDBX(FileInfo[] dbxFiles)
         {
-            var collection = new List<DbxMatches>();
-
-            var dbxFiles = lib.GetFilesOfType(DBX_ROOT, "dbx");
-            foreach (var file in dbxFiles)
+            var allCollections = new List<DbxMatches>();
+            var mutex = new object();
+            Parallel.ForEach(dbxFiles, (file) =>
             {
+                var collection = new List<DbxMatches>();
                 using (var sr = new StreamReader(file.FullName))
                 {
                     var asset = new DbxMatches();
@@ -133,9 +134,65 @@ namespace DbxEntityTracker
                     if (valid)
                         collection.Add(asset);
                 }
+                if (collection.Count > 0)
+                {
+                    lock (mutex)
+                    {
+                        allCollections.AddRange(collection);
+                    }
+                }
+            });
+            return allCollections;
+        }
+
+        private IDictionary<string, List<string>> crossReferenceEntitiesWithDbxMatches(IDictionary<string, string> allEntities, List<DbxMatches> dbxMatches)
+        {
+            var entityUsage = new Dictionary<string, List<string>>(); //Dictionary<EntityReference, DbxFile>
+            foreach (var entity in dbxMatches)
+            {
+                foreach (var t in entity.EntityType)
+                {
+                    if (allEntities.Keys.Contains(t))
+                    {
+                        if (entityUsage.ContainsKey(t))
+                            entityUsage[t].Add(entity.Filepath);
+                        else
+                            entityUsage.Add(t, new List<string>() { entity.Filepath });
+                    }
+                }
             }
 
-            return collection;
+            return entityUsage;
+        }
+
+        private static List<string> findUnusedEntities(IDictionary<string, string> allEntities, IDictionary<string, List<string>> entityUsage)
+        {
+            var unusedEntities = new List<string>();
+            foreach (var e in allEntities)
+            {
+                if (!entityUsage.ContainsKey(e.Key))
+                {
+                    unusedEntities.Add(e.Key);
+                }
+            }
+            return unusedEntities;
+        }
+
+        private void writeUnusedEntities(List<string> unusedEntities)
+        {
+            var sb = new StringBuilder();
+            foreach (var u in unusedEntities)
+            {
+                sb.AppendLine(u);
+            }
+            Console.WriteLine("Unused Entities: {0}", sb.ToString());
+
+            using (var sw = new StreamWriter("../../_docs/unused_entities.txt", false))
+            {
+                sw.Write(sb.ToString());
+                sw.Flush();
+                sw.Close();
+            }
         }
     }
 }
